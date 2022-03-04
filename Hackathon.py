38e05c3754a114fbfc137acb38b866939bac1823
Nalolkol <92116469+Nalolkol@users.noreply.github.com>
2022-03-04 14:59:11 +0100

    Add files via upload

--- Hackathon.py
+++ Hackathon.py
@@ -28,15 +28,16 @@ print(stocks)
 
 
 #%% plot things
-plt.figure()
+'''plt.figure()
 
 for i in range(0,len(tickers)):
     plt.plot(stocks[tickers])
-
+'''
 #%% Main code
 def main():
     while True:
         for ticker in tickers:
+            
             stock = lh.getStockHistory(ticker, 90)
             stock = getmidprices(stock)
             signal = strategy1(stock, ticker)
@@ -45,19 +46,20 @@ def main():
 
 #%% Functions
 
-def getmidprices(indata:pd.DataFrame):
+def getmidprices(indata: pd.DataFrame):
     indata['Open'] = (indata.askOpen + indata.bidOpen)/2
     indata['Low'] = (indata.askLow + indata.bidLow)/2
     indata['Close'] = (indata.askClose + indata.bidClose)/2
     indata['Volume'] = (indata.askVolume + indata.bidVolume)/2
     return indata
 
-def execution(ticker, signals, nbrshares):
-    print(f"\t> Checking signals for {ticker}: {datetime.now()}")
+def execution(ticker, signals):
+    print("Checking signals for {ticker}: {datetime.now()}")
 
     # kolla om marknad öppen
     if lh.getStock(ticker)["askVolume"] > 0:
         portfolio = lh.getPortfolio()
+        valport = lh.getSaldo()
 
         amount = [e["amount"] for e in portfolio
                   if e["symbol"] == ticker][0] if \
@@ -67,25 +69,28 @@ def execution(ticker, signals, nbrshares):
         # sälj om du har
         if amount > 0:
 
-            # You own the stock
-            # Check if you have a sell signal
+            # Kolla säljsignal
             if signals < 0:
-                lh.sellStock(ticker, nbrshares)
-                print(f"\t> Sold {amount} of {ticker} for",
-                      f"{lh.getStock(ticker)['askClose']}")
-                print(f"\t> Time {lh.getStock(ticker)['time']}")
+                lh.sellStock(ticker, amount)
+                print("Sold {amount} of {ticker} for",
+                      "{lh.getStock(ticker)['askClose']}")
+                print("Time {lh.getStock(ticker)['time']}")
 
-        # Buy if you have a buy signal
-        elif signals > 0:
+        # köpsignal
+        
+        elif signals > 0 and valport < 50000 :
             price = lh.getStock(ticker)["bidClose"]
             total_value = lh.getSaldo() +\
                 (sum([e["totalValue"] for e in lh.getPortfolio()])
                  if len(lh.getPortfolio()) > 0 else 0)
             amount = int((total_value * 0.1) / price)
             lh.buyStock(ticker, amount)
-            print(f"\t> Bought {amount} of {ticker} for {price}")
+            lh.placeStoploss(ticker,price*0.95,amount)
+            
+            print("Bought {amount} of {ticker} for {price}")
+            print("Set stopploss {amount} of {ticker} for {price*0.95}")
     else:
-        print(f"\t> {ticker} is closed. {datetime.now()}")
+        print("{ticker} is closed. {datetime.now()}")
         
 def resample_ohlcv(indata: pd.DataFrame, resolution: str):
     return indata.resample(resolution).agg({"Open": "first",
@@ -94,30 +99,31 @@ def resample_ohlcv(indata: pd.DataFrame, resolution: str):
                                             "Close": "last",
                                             "Volume": "sum"}).dropna()
 
-def strategy1(indata:pd.DataFrame, ticker):
+def strategy1(indata: pd.DataFrame, ticker):
     five = resample_ohlcv(getmidprices(indata), "5 min")
     if len(five) < 10:
-        print("Not enough data.")
+        print("Not enough data")
         return None
     
     ema_short = five.Close.rolling(25).ewm(span=25, adjust=False).mean()
+    ema_long = five.Close.rolling(100).ewm(span=100, adjust=False).mean()
     ma_volume_short = five.Close.rolling(25).mean()
-    if ema_short[-1] < five.Close[-1] and ma_volume_short[-1] < five.Volume[-1]*5:
+    if ema_short[-1] < five.Close[-1] and ma_volume_short[-1] < five.Volume[-1]*2 and ema_long[-1] < five.Close[-1]:
         return 1
-    elif ema_short[-1] > five.Close[-1]:
+    elif ema_long[-1] > five.Close[-1]:
         return -1
     else:
         return 0
     
 #%% Plot
-
+'''
 plt.figure1()
 
 for i in range(0,len(tickers)):
     stockdata = data[data['symbol']==tickers[i]]
     stockdata.reset_index(drop=True, inplace=True)
     plt.plot(stockdata['bidClose'])
-
+'''
 
 
 
